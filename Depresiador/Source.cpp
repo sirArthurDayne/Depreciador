2715d50dda6749d9c340456413fdec2633009425
Xavier Lamela(Krosis) <xavierivan2000@hotmail.com>
2018-06-09 22:11:43 -0500

    Cambios por Xavier: 1. Crear el algoritmo principal

---
+++ Depresiador/Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+/*
+	Programa que calcula depresiacion de articulos basandose en su periodo de vida util.
+	Usando Metodo de suma de digitos
+	Algoritmo creado por Johan y Xavier.
+*/
+int main()
+{
+	float vidaUtil;//periodo util de articulos en años
+	float precioOriginal, factor,caidaAnual;
+
+	//Entrada de datos
+	std::cout << "Ingrese coste total del articulo: ";
+	std::cin >> precioOriginal;
+
+	std::cout << "Ingrese util de articulos en años: ";
+	std::cin >> vidaUtil;
+
+	//calculo de suma de vidaUtil
+	float sumaVidaUtil = ( vidaUtil * (vidaUtil + 1) ) / 2;
+
+	//iterador
+	int n = vidaUtil;
+
+	//Calculo
+	for (int i = 0; i < n; i++)
+	{
+
+		factor = vidaUtil / sumaVidaUtil; //calcular el factor de depresion
+		caidaAnual = factor * precioOriginal; //perdida del valor en ese año
+		precioOriginal -= caidaAnual;//total tras restar la caida
+		vidaUtil--;//le restamos 1 año antes de iterar
+
+	}
+
+}
